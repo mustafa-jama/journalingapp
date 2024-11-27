ba6d566c85dac847a4f0eb7142ab75af6556de4c
Mustafa Jama <mustafajama273@gmail.com>
2024-11-26 21:35:56 -0600

    Complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { useState } from "react"
 export default function App() {
   const [taskList, setTaskList] = useState([])
   const [currentTask, setCurrentTask] = useState("")
+  const [editTask,setEditTask] = useState(null)
+  const[isEditActive,setIsEditActive] = useState(false)
+
 
   const AddTask = (currentTask) => {
     if (currentTask.length < 1) {
@@ -13,6 +16,39 @@ export default function App() {
     newList.push(currentTask)
     setTaskList(newList)
     setCurrentTask("")
+  }
+
+  const handleDelete = (indexRemove) =>{
+
+      const newList = taskList.filter((_ , index) => index !==  indexRemove)
+      setTaskList(newList)
+
+      // const newList = [...taskList]
+      // newList.filter((_ , index) => index !==  indexRemove)
+      // setTaskList(newList)
+
+  }
+
+  
+  const handleEdit = (task , index) =>{
+
+    setEditTask({text:task,index:index})
+    console.log(editTask)
+    
+    setIsEditActive(true)
+
+
+  }
+
+  const handleConfirm = () =>{
+
+    const newList = [...taskList]
+    newList[editTask.index] = editTask.text
+    setTaskList(newList)
+    setIsEditActive(false)
+    console.log(editTask.text)
+
+
   }
 
   return (
@@ -38,21 +74,58 @@ export default function App() {
 
       <div>
         {taskList.map((task, index) => (
-          <Task task={task} key={index} />
+          <Task task={task} key={index} 
+          index={index}
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}/>
+
         ))}
       </div>
+
+      { isEditActive && (
+
+        <div> 
+
+          <input
+
+            className='editTask'
+            type='text'
+            onChange={(event) => setEditTask({index: editTask.index, text: event.target.value})}
+            value={editTask.text}
+
+          />
+
+        <button 
+        onClick={handleConfirm}
+        > Confirm </button>
+
+        </div>
+
+      )}
+
     </div>
   )
+
+  
+
+  
+
+
 }
 
-const Task = ({ task }) => {
+const Task = ({ task , index, handleDelete, handleEdit }) => {
   return (
     <div className='taskLayout'>
-      <p  className='taskColor'> {task} </p>
+      <p className='taskColor'> {task} </p>
 
       <div className='butttonLayout'>
-        <button className='editButton'>Edit</button>
-        <button className='deleteButton'>Delete</button>
+        <button
+        onClick={() => handleEdit(task,index)}
+
+        className='editButton'>Edit</button>
+        <button 
+        onClick={() => handleDelete(index)}
+        className='deleteButton'>Delete</button>
       </div>
     </div>
   )
